0b0bbf13622e3d198432400f72aab28502cb7183
Rafael Campo <rafael.campo.dev@gmail.com>
2024-07-31 11:32:53 -0400

    docs babaska tasks scripts done

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # gold-accountant
+
+# Example Usage
+
+To run the examples you must be able to run babashka from the command line.
+
+> *Babashka* is a native Clojure interpreter for scripting with fast startup.
+
+> Clojure is a concise, powerful, and performant general-purpose  programming language that runs on the Java Virtual Machine, Common Language Runtime, JavaScript runtimes (aka Node.js or modern mobile/desktop web browsers), and Babashka.
+
+See the [Babashka documentation](https://github.com/babashka/babashka#installation) for more information
+
+## Functions
+
+There are two functions
+
+- **list**: Returns an itemized list of the assets contained within the gold.edn file
+- **total**: Returns the sum of the dollar value of all assets
+
+## Run examples as a Babaska Task
+
+### **List** - as a Babashka Task
+
+`bb run list`
+
+```
+Output
+
+[1.0 2431.4 :gold American Double Eagle]
+[1 2431.4 :gold American Double Eagle]
+[1 2431.4 :gold Canadian Mapel Leaf]
+```
+
+![](screenshots/task-list.png)
+
+### **Total** - as a Babashka Task
+
+bb run total
+
+`bb run total` Output: `$7294.20`
+
+![](screenshots/task-total.png)
+
+## Run examples as a shell script
+
+### **List** - as shell script
+
+`./gold.clj list`
+
+```
+Output
+
+[1.0 2431.4 :gold American Double Eagle]
+[1 2431.4 :gold American Double Eagle]
+[1 2431.4 :gold Canadian Mapel Leaf]
+```
+
+![](screenshots/script-list.png)
+
+### **Total** - as shell script
+
+`./gold.clj total` Output: `$7294.20`
+
+![](screenshots/script-total.png)
+
+---
+
+### Demo of all functionality
+
+![](screenshots/all-cli-report.png)
--- gold.clj
+++ gold.clj
@@ -1,93 +1,64 @@
 #!/usr/bin/env bb
 
-(require '[babashka.http-client :as http]
-         '[cheshire.core :refer :all])
+
+(defn read-edn-file [filename]
+  (with-open [reader (io/reader filename)]
+    (edn/read (java.io.PushbackReader. reader))))
+
+;; (def data (read-edn-file "gold.edn"))
+
+;; (prn data)
+
+(def metals-prices (read-edn-file "prices.edn"))
 
 
-(defn get-url [url]
-  (println "Downloading url:" url)
-  (spit "XAU-USD-qoutex.json" (:body (http/get url))))
+;; (def metals-prices {
+;;                     ;; :us-gold 2441.40 ; Mon Jul 15 05:57:38 PM EDT 2024
+;;                     ;; :us-silver 31.01 ; Mon Jul 15 10:51:50 PM EDT 2024
 
-;; Next Steps
-; - include calculation of records in which their weight is in grams
-; - add documentation to the functions
-; - add testing
-; - wrap this functionality in babashka tasks
+;;                     :us-gold 2431.40 ; Wed Jul 31 09:16:12 AM EDT 2024
+;;                     :us-silver 28.86 ; Wed Jul 31 09:16:12 AM EDT 2024
+;; })
 
-
-;; Now what is the equation, the formula, the function that shows me how much in pounds my gold and silver is worth?
-;; If I convert that back into dollars is the same? 
-;; If not then let enjoy my depth and breath as I take my fortune. Character, scripts, mind, soul, house, spirit, trees, plants, flowers, bring me fortune.
+(def metals  (read-edn-file "gold.edn"))
 
 
-(def metals-prices {
-                    
-                    ;; :us-gold (get-url "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD")
-                    
-
-                    ;; :us-gold 2441.40 ; Mon Jul 15 05:57:38 PM EDT 2024
-                    ;; ;; :us-silver 30.86 ; Mon Jul 15 05:57:38 PM EDT 2024
-                    ;; :us-silver 31.01 ; Mon Jul 15 10:51:50 PM EDT 2024
-
-                    :us-gold 2431.40 ; Wed Jul 31 09:16:12 AM EDT 2024
-                    :us-silver 28.86 ; Wed Jul 31 09:16:12 AM EDT 2024
-
-
-})
-
-
-;; works
-;; (def to-json-file (spit "XAU-USD-qoutex.json" (:body (http/get "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"))))
+;; (def metals
+;;   [
+;; [2 0.5 "American Double Eagle" :ounces :gold]
+;;    [1 1 "American Double Eagle" :ounces :gold]
+;;    [1 1 "Canadian Mapel Leaf" :ounces :gold]
+;;    [4 0.5 "Mexican Silver Libertad" :ounce :silver]
+;;    [1 5 "Alphonse Mucha" :ounce :silver]
+;;    [1 5 "Alphonse Mucha" :ounce :silver]
+;;    [1 1 "American Silver Eagle" :ounce :silver]
+;;    [6 1 "Apmex 1 ounce generic" :ounce :silver]
+;;    [2 1 "Silver Britania" :ounce :silver]
+;;    [1 1 "Mayan Silver Coin" :ounce :silver]
+;;    [1 2 "The Kraken Silver" :ounce :silver]
+;;    ])
 
 
-(def from-json (cheshire.core/parse-string (slurp "XAU-USD-qoutex.json") true))
 
-;; (println (get-url "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"))
+;; (require '[clojure.edn :as edn]
+;;          '[clojure.java.io :as io])
 
-;; (println (get (:body (http/get "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD")) 6 ) )
+;; (def data {:name "John Doe"
+;;            :age 30
+;;            :email "john.doe@example.com"})
 
-;; (def runtime-cache (cheshire.core/parse-string (:body (http/get "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"))))
-;; (println (get runtime-cache 0))
+;; (defn write-edn-file [filename data]
+;;   (with-open [writer (io/writer filename)]
+;;     (binding [*out* writer]
+;;       (prn data))))
 
-(println (type from-json))
+;; (write-edn-file "prices.edn" metals-prices)
 
-(println "Gold:" (nth from-json 6)) ; https://www.reddit.com/r/webdev/comments/axkvck/comment/ehzf51u/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 
-(println "Gold spread profile prices" (:spreadProfilePrices (nth from-json 6))) ; https://www.reddit.com/r/webdev/comments/axkvck/comment/ehzf51u/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 
-(doseq [spp (:spreadProfilePrices (nth from-json 6))]
-  (println spp))
 
-;; (doseq [rc from-json]
-;;   (println "rc:" rc))
 
-;; (println (get from-json 1))
 
-;; (println (get (:body (http/get "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD")) 0 ) )
-
-;; (doseq [b  (:body (http/get "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"))]
-;;   (println b))
-
-;; (println (cheshire.core/parse-string {:foo "bar" :baz {:eggplant [1 2 3]}} {:pretty true}))
-
-;; The metal's vector may contain 0 or many unit-vectors. 
-;; Unit-Vectors describe a bullion or coin in the following order:
-;; quantity - weight-per-unit (ounces or grams) - Name - metal
-(def metals
-  [
-[2 0.5 "American Double Eagle" :ounces :gold]
-   [1 1 "American Double Eagle" :ounces :gold]
-   [1 1 "Canadian Mapel Leaf" :ounces :gold]
-   ;; [1 7 "Vespian" :grams :gold]
-   [4 0.5 "Mexican Silver Libertad" :ounce :silver]
-   [1 5 "Alphonse Mucha" :ounce :silver]
-   [1 5 "Alphonse Mucha" :ounce :silver]
-   [1 1 "American Silver Eagle" :ounce :silver]
-   [6 1 "Apmex 1 ounce generic" :ounce :silver]
-   [2 1 "Silver Britania" :ounce :silver]
-   [1 1 "Mayan Silver Coin" :ounce :silver]
-   [1 2 "The Kraken Silver" :ounce :silver]
-   ])
 
 (defn total-weight [quantity weight]
   (* quantity weight))
@@ -120,24 +91,19 @@
 
                  (get v 2) ; Description
                  ]
-                
                 ) metals))
 
 
-(doseq [ob open-bag]
-  (println ob))
+(defn -main [function] 
+  (cond (= "help" function) (println "Available commands: list, total, help")
+        (= "list" function) (doseq [ob open-bag] (println ob))
+        (= "total" function) (println (str "$" (reduce (fn [acc v] (+ acc (gold-silver-or v metals-prices))) 0 metals)))
 
+        ;; More functions to follow
 
-(println "Total Metals:"  (str "$" (reduce (fn [acc v] (+ acc (gold-silver-or v metals-prices))) 0 metals)))
+        (= nil function) (println "Error. You must specify a function.")
 
+        :else (println "Invalid command. Use 'help' for the list of available commands."))
+  )
 
-
-
-
-;;;; Notes
-;;
-
-;; equivalent to + in reduce because + take two argumnts.
-;; Inside the reduce fucntion the first argument is the accumulator and the second is the value
-;; (defn my-func [acc m] 
-;;   (+ acc (gold-silver-or m)))
+(-main (first *command-line-args*))
